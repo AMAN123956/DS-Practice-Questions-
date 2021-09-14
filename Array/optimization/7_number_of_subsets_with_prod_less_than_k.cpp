1a2636d4d53d04c479224b5f57c0deb476f1f6d7
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-09-14 17:43:21 +0530

    number of subsets with prod less than or equal to k

---
+++ Array/optimization/7_number_of_subsets_with_prod_less_than_k.cpp
@@ -0,0 +1,77 @@
+// Smallest subarray with sum greater than k
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define rep(i, a, b) for (ll i=a; i<b; ++i)
+typedef long long ll;
+typedef vector<ll> vll;
+// Array is in descending order
+int bsearch(int arr[],int l,int h,int pivot){
+    int mid=0;
+// cout<<l<<h<<k<<"\n";
+
+   while(l<=h){
+       mid=l+(h-l)/2;
+    //  cout<<mid<<arr[mid];
+       if(arr[mid]==pivot) {
+        //   cout<<"here";
+           return mid;
+           break;
+       }
+       else if(arr[mid]>pivot){
+          l=mid+1;
+          continue;
+          
+       }
+       else{
+           h=mid-1;
+           continue;
+       }
+   }
+   
+   return -1;
+   
+}
+// Struct to return multiple values from function
+struct Pair{
+    int min;
+    int max;
+};
+
+void solve(){
+    int n,k,prod=1,sprod=1,count=0;
+    cin>>n>>k;
+    int arr[n];
+    rep(i,0,n) cin>>arr[i];
+    
+    rep(i,0,n){
+        if(arr[i]<=k) count++;
+    }
+    
+    rep(i,0,n){
+        prod=arr[i];
+        rep(j,i+1,n){
+            sprod=arr[i]*arr[j];
+            // cout<<sprod<<" "<<i<<" "<<j;
+            // cout<<"\n";
+            prod=prod*arr[j];
+            if(prod<=k || sprod<=k) count++;
+        }
+    }
+    cout<<count;
+    return;
+    
+}
+int main()
+{
+ ll t;
+ cin>>t;
+ while(t--)
+ solve();
+ 
+  return 0;
+}
+
+// Time Complexity: O(2^n)
+// Space Complexity: O(1)
