55b3dba9247afce7436149bca4bfa9dd75f711b9
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-09-18 18:09:18 +0530

    pivot index

---
+++ leetcode/11_find_pivot_index.cpp
@@ -0,0 +1,29 @@
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        long left_sum=0;
+        long right_sum=0;
+        int index=0;
+        for(int i=1;i<nums.size();i++){
+            right_sum+=nums[i];
+        }
+        // initially considering 1st index as pivot
+        while(index<nums.size()){
+            if(left_sum==right_sum){
+                return index;
+            }
+            else{
+                left_sum+=nums[index];
+                index+=1;
+                right_sum-=nums[index];
+            }
+        }
+        return -1;
+    }
+};
