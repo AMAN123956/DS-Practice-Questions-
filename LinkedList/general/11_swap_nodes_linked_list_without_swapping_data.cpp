1cfe5702597e20265c0063f01c296bb0667da579
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-10-22 10:24:29 +0530

    swap nodes in linked list without swapping data

--- LinkedList/general/11_swap_nodes_linked_list_without_swapping_data.cpp
+++ LinkedList/general/11_swap_nodes_linked_list_without_swapping_data.cpp
@@ -1,98 +1,91 @@
-// C++ program to swap two given nodes of a linked list
-#include <iostream>
-
+/* This program swaps the nodes of linked list rather
+than swapping the field from the nodes.*/
+#include <bits/stdc++.h>
 using namespace std;
 
-// A linked list node class
+/* A linked list node */
 class Node {
-
 public:
 	int data;
-	class Node* next;
-
-	// constructor
-	Node(int val, Node* next)
-		: data(val)
-		, next(next)
-	{
-	}
-
-	// print list from this
-	// to last till null
-	void printList()
-	{
-
-		Node* node = this;
-
-		while (node != NULL) {
-
-			cout << node->data << " ";
-			node = node->next;
-		}
-
-		cout << endl;
-	}
+	Node* next;
 };
 
-// Function to add a node
-// at the beginning of List
-void push(Node** head_ref, int new_data)
-{
-
-	// allocate node
-	(*head_ref) = new Node(new_data, *head_ref);
-}
-
-void swap(Node*& a, Node*& b)
-{
-
-	Node* temp = a;
-	a = b;
-	b = temp;
-}
-
+/* Function to swap nodes x and y in linked list by
+changing links */
 void swapNodes(Node** head_ref, int x, int y)
 {
-
 	// Nothing to do if x and y are same
 	if (x == y)
 		return;
 
-	Node **a = NULL, **b = NULL;
-
-	// search for x and y in the linked list
-	// and store therir pointer in a and b
-	while (*head_ref) {
-
-		if ((*head_ref)->data == x) {
-			a = head_ref;
-		}
-
-		else if ((*head_ref)->data == y) {
-			b = head_ref;
-		}
-
-		head_ref = &((*head_ref)->next);
+	// Search for x (keep track of prevX and CurrX
+	Node *prevX = NULL, *currX = *head_ref;
+	while (currX && currX->data != x) {
+		prevX = currX;
+		currX = currX->next;
 	}
 
-	// if we have found both a and b
-	// in the linked list swap current
-	// pointer and next pointer of these
-	if (a && b) {
+	// Search for y (keep track of prevY and CurrY
+	Node *prevY = NULL, *currY = *head_ref;
+	while (currY && currY->data != y) {
+		prevY = currY;
+		currY = currY->next;
+	}
 
-		swap(*a, *b);
-		swap(((*a)->next), ((*b)->next));
+	// If either x or y is not present, nothing to do
+	if (currX == NULL || currY == NULL)
+		return;
+
+	// If x is not head of linked list
+	if (prevX != NULL)
+		prevX->next = currY;
+	else // Else make y as new head
+		*head_ref = currY;
+
+	// If y is not head of linked list
+	if (prevY != NULL)
+		prevY->next = currX;
+	else // Else make x as new head
+		*head_ref = currX;
+
+	// Swap next pointers
+	Node* temp = currY->next;
+	currY->next = currX->next;
+	currX->next = temp;
+}
+
+/* Function to add a node at the beginning of List */
+void push(Node** head_ref, int new_data)
+{
+	/* allocate node */
+	Node* new_node = new Node();
+
+	/* put in the data */
+	new_node->data = new_data;
+
+	/* link the old list off the new node */
+	new_node->next = (*head_ref);
+
+	/* move the head to point to the new node */
+	(*head_ref) = new_node;
+}
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node)
+{
+	while (node != NULL) {
+		cout << node->data << " ";
+		node = node->next;
 	}
 }
 
-// Driver code
+/* Driver program to test above function */
 int main()
 {
-
 	Node* start = NULL;
 
-	// The constructed linked list is:
-	// 1->2->3->4->5->6->7
+	/* The constructed linked list is:
+	1->2->3->4->5->6->7 */
 	push(&start, 7);
 	push(&start, 6);
 	push(&start, 5);
@@ -102,10 +95,14 @@ int main()
 	push(&start, 1);
 
 	cout << "Linked list before calling swapNodes() ";
-	start->printList();
+	printList(start);
 
-	swapNodes(&start, 6, 1);
+	swapNodes(&start, 4, 3);
 
-	cout << "Linked list after calling swapNodes() ";
-	start->printList();
+	cout << "\nLinked list after calling swapNodes() ";
+	printList(start);
+
+	return 0;
 }
+
+// This is code is contributed by rathbhupendra
