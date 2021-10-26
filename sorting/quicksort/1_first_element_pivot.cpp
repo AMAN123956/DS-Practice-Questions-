0da4b8f40842f1c16dbdfe4d4c9a436bf57f40ae
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-10-26 15:15:19 +0530

    first element pivot

---
+++ sorting/quicksort/1_first_element_pivot.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        quickSort(nums,low,high);
+        return nums;
+    }
+ public:
+    void quickSort(vector<int>& nums,int low,int high){
+        if(low==high || low>high) return;
+        
+        int pivot=nums[low];
+        int pi=partition(nums,low,high,pivot);
+        quickSort(nums,low,pi-1);
+        quickSort(nums,pi+1,high);
+    }
+  public:
+    int partition(vector<int>& nums,int low,int high,int pivot){
+        
+        int i=high;
+        int j=high;
+        while(i>=0){
+            if(nums[i]<pivot){
+                i--;
+            }
+            else{
+                swap(nums[i],nums[j]);
+                i--;
+                j--;
+            }
+        }
+        cout<<nums[j+1]<<"\n";
+        return j+1;
+    }
+};
