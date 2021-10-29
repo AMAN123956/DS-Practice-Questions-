1d7f2c57ab01d02d77051c66e5e5572da0cf3c53
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-10-29 16:27:54 +0530

    next greater element in normal array

---
+++ leetcode/stack/36(a)_next_greater_element_normal_array.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long> ans;
+        stack<long long> st;
+        ans.push_back(-1);
+        st.push(arr[arr.size()-1]);
+        for(int i=arr.size()-2;i>=0;i--){
+            while(st.size()>0 && arr[i]>=st.top()){
+                st.pop();
+            }
+            if(st.size()==0) ans.push_back(-1);
+            else ans.push_back(st.top());
+            
+            st.push(arr[i]);
+        }
+        // as we are storing in vector
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
