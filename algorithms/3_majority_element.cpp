56d38ab99cb36bd6af597e54666a4831d4a62acc
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2022-07-11 16:44:35 +0530

    updated info related to alog

--- algorithms/3_majority_element.cpp
+++ algorithms/3_majority_element.cpp
@@ -1,4 +1,5 @@
 Problem Link: https://leetcode.com/problems/majority-element/
+// Moore Voting Algorithm
 
 class Solution {
 public:
@@ -8,6 +9,7 @@ public:
    int ans=A[0];
    int count=1;
     
+   // To find majority element
    for(int i=1;i<A.size();i++){
        if(ans==A[i]) count++;
        else if(ans!=A[i]) count--;
@@ -18,7 +20,8 @@ public:
        }
         
    }
-        
+    
+    // To check if majority element is actually a majority element
     count=0;
     for(int i=0;i<A.size();i++){
         if(A[i]==ans) count++;
